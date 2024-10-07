b0d7ae9a35aa6af8bf1fc479ef59b7aefb1d6649
eliasprivadoe <eliasprivado@byui.edu>
2024-10-07 14:54:10 -0400

    Added files for Learning02

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        //Adding Job class to main program
+        //First job
+        Job job1 = new Job();
+
+        job1._company = "Microsoft";
+        job1._jobTitle = "Software Engineer";
+        job1._startYear = 2020;
+        job1._endYear = 2024;
+
+        job1.Display();
+
+        //Second job
+        Job job2 = new Job();
+
+        job2._company = "Apple";
+        job2._jobTitle = "Designer";
+        job2._startYear = 2018;
+        job2._endYear = 2021;
+
+        job2.Display();
+
+        //Adding Resume class to main program
+        //newResume._jobs.Add(job1);
     }
 }
---
+++ prepare/Learning02/Resume.cs
@@ -0,0 +1,13 @@
+//Resume class
+public class Resume
+{
+    //Attributes
+    public string _name  = "";
+    public List<Job> _jobs = new List<Job>();
+
+    //Methods
+    public void Display()
+    {
+        Console.WriteLine();
+    }
+}
